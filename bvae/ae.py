596bd1e9841ae8e8135873a7db03f357e8209093
stoplime <slanimero@gmail.com>
2018-04-26 23:16:23 -0400

    copy working model

--- bvae/ae.py
+++ bvae/ae.py
@@ -60,6 +60,7 @@ def test():
         pred = Image.fromarray(pred[0])
         pred.show() # display popup
 
+# The working Dsprite3_2 model
 def test2():
     import os
     import numpy as np
@@ -175,5 +176,120 @@ def test2():
             # pred = Image.fromarray(pred[0])
             # pred.show() # display popup
 
+def test3():
+    import os
+    import numpy as np
+    from PIL import Image
+    from tensorflow.python.keras.preprocessing.image import load_img
+    import cv2
+    import models
+    import random
+
+    inputShape = (64, 64, 1)
+    batchSize = 32
+    latentSize = 10
+    episodes = 10000
+    verbose = 1
+
+    loadFolder = 'imageNet2'
+    loadFile = 'PtBetaEncoder-32px-128l-1000e'
+    load = False
+    saveFolder = 'Dsprite3_2'
+    saveFile = 'PtBetaEncoder-bvae-0_1-10l-20c-64px-10000e'
+    save = True
+
+    useDsprites = True
+
+    # C:\Users\slani\Documents\GitHub\montazuma\dataset\0000001.png
+    # C:\Users\slani\Documents\GitHub\montazuma\dataset\1281149.png
+    # dataPath = os.path.join('..', '..', 'dataset', 'train_32x32')
+    dataPath = os.path.join('..', 'dataset')
+
+    loadFolderPath = os.path.join('..', 'save', loadFolder)
+    saveFolderPath = os.path.join('..', 'save', saveFolder)
+    loadPath = os.path.join(loadFolderPath, loadFile + '.h5')
+    savePath = os.path.join(saveFolderPath, saveFile + '.h5')
+
+    if useDsprites:
+        print("loading images")
+        data = np.load(os.path.join(dataPath, "dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz"))
+        dataImages = data['imgs']
+        print("dataImages.shape", dataImages.shape)
+        print("Images Aquired")
+    
+    if not os.path.exists(saveFolderPath):
+        os.makedirs(saveFolderPath)
+
+    # This is how you build the autoencoder
+    encoder = models.BetaEncoder(inputShape, batchSize, latentSize, 'bvae', beta=0.1, capacity=20, randomSample=True)
+    decoder = models.BetaDecoder(inputShape, batchSize, latentSize)
+    bvae = AutoEncoder(encoder, decoder)
+
+    bvae.ae.compile(optimizer='adam', loss='mean_absolute_error')
+
+    if load:
+        bvae.ae.load_weights(loadPath)
+
+    for epoch in range(episodes):
+        imgs = []
+        for _batch in range(batchSize):
+            if useDsprites:
+                imageNum = random.randrange(1, 737280)
+                img = dataImages[imageNum]
+                img = img*255
+                imgs.append(img)
+            else:
+                imageNum = str(random.randrange(1, 1281150)).zfill(7)
+                img = load_img(os.path.join(dataPath, imageNum+".png"), target_size=inputShape[:-1])
+                imgs.append(np.array(img, dtype=np.uint8))
+
+        
+        if verbose == 1:
+            batch_view = np.array(imgs, dtype=np.uint8)
+            if useDsprites:
+                batch_view = np.expand_dims(batch_view, axis=3)
+            print("batch.shape", batch_view.shape)
+            visualize_batch = np.concatenate((*batch_view,), axis=1)
+            # visualize_batch = cv2.cvtColor(visualize_batch, cv2.COLOR_RGB2BGR)
+            cv2.imshow("batch", visualize_batch)
+            cv2.waitKey(1)
+        
+        batch = np.array(imgs, dtype=np.float32)
+        if useDsprites:
+            batch = np.expand_dims(batch, axis=3)
+        batch = batch / 255 - 0.5
+
+        bvae.ae.fit(batch, batch,
+                    epochs=100,
+                    batch_size=batchSize)
+        
+        # example retrieving the latent vector
+        if verbose == 1:
+            latentVec = bvae.encoder.predict(batch)[0]
+            print(latentVec)
+
+        print("episode: {}/{}".format(epoch+1, episodes))
+        if save:
+            bvae.ae.save_weights(savePath)
+
+        if verbose == 1:
+            pred = bvae.ae.predict(batch) # get the reconstructed image
+            print("pred.shape", pred.shape)
+            pred[pred > 0.5] = 0.5 # clean it up a bit
+            pred[pred < -0.5] = -0.5
+            pred = np.uint8((pred + 0.5)* 255) # convert to regular image values
+
+            visualize_pred = np.concatenate((*pred,), axis=1)
+            if not useDsprites:
+                visualize_pred = cv2.cvtColor(visualize_pred, cv2.COLOR_RGB2BGR)
+            visualize_both = np.concatenate((visualize_batch, visualize_pred), axis=0)
+
+            cv2.imwrite(os.path.join(saveFolderPath, "sample_{}.png".format(str(epoch).zfill(7))), visualize_both)
+            cv2.imshow("prediction", visualize_both)
+            cv2.waitKey(1)
+
+            # pred = Image.fromarray(pred[0])
+            # pred.show() # display popup
+
 if __name__ == "__main__":
-    test2()
+    test3()
